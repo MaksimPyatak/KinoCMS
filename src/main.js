baba7bf46c946fd3e92a6efc7a82fcc7a76df5fd
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-04-24 23:11:17 +0300

    adding dist

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { app } from './app'
+//import { app } from './app'
+import { createApp } from 'vue' //!!!!!
+import { createPinia } from 'pinia'
 
+import App from '@/app/index.vue'
+import { router } from '@/app/providers'
+//import './styles/admin-lte-style.scss'
+import '@/app/index.scss';
+
+const app = createApp(App).use(createPinia()).use(router)
 
 app.mount('#app')
